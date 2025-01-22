c48e567af736d82c414bf44cbff63d1e1eaf6bfb
YogeshBadgujar09 <yogeshsb18@gmail.com>
2025-01-22 11:23:59 +0530

    [ADDED] : Yogesh - My first android project

---
+++ MyFirstProject/app/src/main/java/com/yogesh/myfirstproject/DemoLayout.java
@@ -0,0 +1,17 @@
+package com.yogesh.myfirstproject;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class DemoLayout extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.demo_layout);
+
+    }
+}
