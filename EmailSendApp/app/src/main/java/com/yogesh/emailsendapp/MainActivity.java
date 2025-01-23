8efad7e8014da651aed65cb7530fb18910e07ede
YogeshBadgujar09 <yogeshsb18@gmail.com>
2025-01-23 23:37:02 +0530

    [MODIFIED] : Sending Email Problem --- Face Exception While Sending mail

--- EmailSendApp/app/src/main/java/com/yogesh/emailsendapp/MainActivity.java
+++ EmailSendApp/app/src/main/java/com/yogesh/emailsendapp/MainActivity.java
@@ -1,6 +1,10 @@
 package com.yogesh.emailsendapp;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
@@ -11,5 +15,23 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main_layout);
+    }
+
+    public void sendEmail(View v)
+    {
+        EditText to = findViewById(R.id.txtTo);
+        EditText subject = findViewById(R.id.txtSubject);
+        EditText message = findViewById(R.id.txtComposeMail);
+
+        Intent i = new Intent(Intent.ACTION_SEND);
+        i.setData(Uri.parse("mailto"));
+
+        i.putExtra(Intent.EXTRA_EMAIL, new String[]{to.toString()});
+        i.putExtra(Intent.EXTRA_SUBJECT,subject.toString());
+        i.putExtra(Intent.EXTRA_TEXT,message.toString());
+
+        startActivity(i);
+
+
     }
 }
